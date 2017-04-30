c8c7a8dedd73b066d80a1d0ccf6d461d2d400f2c
danghualong <dhl_345@qq.com>
2017-04-30 22:37:31 +0800

    use iterator pattern to valid the raw data

--- RawDataParser/RawDataParser/RawDataParser.py
+++ RawDataParser/RawDataParser/RawDataParser.py
@@ -3,64 +3,74 @@ import numpy as  np
 import struct
 
 map={1:'PW1',16:'CW',-32768:'PW2'}
+#The length of the scan line
+scanLineSizes={}
+#The next scan line number 
+expectedLineNOs={}
+#The total valid bytes for the scanline
+validNums={}
+#The latest content in the packet
+chData={} 
+
 
 def readData(fileName):
-    chData={}
+    blockSize=512
     with open(fileName,'rb') as f:
         while True:
-            data=f.read()
-            arrLen=len(data)
-            if arrLen<=0:
-                break
+            data=f.read(blockSize)
+            if data:
+                yield data
+            else:
+                return
 
-            for i in range(0,arrLen,512):
-                #get the first element in the tuple??the return value of unpack_from is tuple)
-                chNO=struct.unpack_from('<h',data,i+24)[0]
-                if not chData.has_key(chNO):
-                    chData[chNO]=[[],[]]
-                for j in range(i+36,i+268,4):
-                    item=struct.unpack_from('<l',data,j)[0]
-                    chData[chNO][0].append(item)
-                for j in range(i+268,i+500,4):
-                    item=struct.unpack_from('<l',data,j)[0]
-                    chData[chNO][1].append(item)
-    return chData
+def saveData(data):
+    #get the first element in the tuple??the return value of unpack_from is tuple)
+    chNO=struct.unpack_from('<h',data,24)[0]
+    if not chData.has_key(chNO):
+        chData[chNO]=[[],[]]
+        validNums[chNO]=-1
+    #The data in I's Part
+    for j in range(36,268,4):
+        item=struct.unpack_from('<l',data,j)[0]
+        chData[chNO][0]=data;
+    #The data in Q's Part
+    for j in range(268,500,4):
+        item=struct.unpack_from('<l',data,j)[0]
+        chData[chNO][1]=data
+    return chNO
 
-def invalidData(chNO,partKey,iqData):
-    headNO=0
-    expectHeadNO=0
-    size=0
-    startIdx=0
-    lineNOs=[]
-    for i in range(len(iqData)):
-        if iqData[i]==0x7FFF and iqData[i+1]==0:
-            startIdx=i
-            size=iqData[i+3]
-            headNO=iqData[i+2]
-            expectHeadNO=headNO
-            print ("channel %s %s part's init scanLineNO is %s" %(map[chNO],partKey,hex(headNO)))
-            break
-    for i in range(startIdx,len(iqData),size):
-        if iqData[i]==0x7FFF and iqData[i+1]==0:
-            headNO=iqData[i+2]
-            lineNOs.append(hex(headNO))
-            if expectHeadNO!=headNO:
-                print ("expect headNO is %s,actual headNO is %s" %(hex(expectHeadNO),hex(headNO)))
+def validateData(chNO,partKey):
+    nPart=(0 if partKey=="I" else 1)
+    iqData=chData[chNO][nPart]
+    if(validNums[chNO]==-1):
+        for i in range(len(iqData)):
+            if iqData[i]==0x7FFF and iqData[i+1]==0:
+                expectedLineNOs[chNO]=iqData[i+2]
+                scanLineSizes[chNO]=iqData[i+3]
+                validNums[chNO]=len(iqData)-i
                 break
-            expectHeadNO+=1
-        else:
-            print("!!!!Not Line Header after LineNO %s" %(hex(expectHeadNO-1)))
-            break
-    #print lineNOs
+    else:
+       validNums[chNO]+=len(iqData)
+       if(validNums[chNO]>scanLineSizes[chNO]):
+           startIdx=len(iqData)+scanLineSizes[chNO]-validNums[chNO]
+           if iqData[startIdx]==0x7FFF and iqData[startIdx+1]==0:
+               headNO=iqData[startIdx+2]
+               expectHeadNO=(expectedLineNOs[chNO]+1)%0xFFFF
+               if expectHeadNO!=headNO:
+                   print("In channel part(%d,%s): expect headNO is %s,actual headNO is %s" %(chNO,partKey,hex(expectHeadNO),hex(headNO)))
+                   return
+               else:
+                   expectedLineNOs[chNO]=expectHeadNO
+                   validNums[chNO]-=scanLineSizes[chNO]
+           else:
+                print("!!!!Not Line Header after LineNO %d for channel %d" %(hex(expectedLineNOs[chNO]),chNO))
+                return
 
 
 if __name__=="__main__":
-    chData=readData(r'D:\allFile.dat')
-    print "read data completed..."
-    
-    keys=chData.keys()
-    for i in range(len(keys)):
-        invalidData(keys[i],"I",chData[keys[i]][0])
-        invalidData(keys[i],"Q",chData[keys[i]][1])
+    for data in readData(r'D:\allFile.dat'):
+        chNO=saveData(data)
+        validateData(chNO,"I")
+        validateData(chNO,"Q")
     print "valid completed"
         
