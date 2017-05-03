af2768a12a06c91be62ba8b418453af66eb3890f
dhl_345 <dhl_345@qq.com>
2017-05-03 08:34:57 +0800

    Add InteractiverConsole.csproj

---
+++ InterpreterDemo/InteractiverConsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InteractiverConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScriptEngine engine = Python.CreateEngine();
+            ScriptScope scope = engine.CreateScope();
+            //Student stu = new Student("LiLei", 30);
+            //scope.SetVariable("stu", stu);
+            ScriptSource src = engine.CreateScriptSourceFromFile("drawer.py");
+            src.Execute(scope);
+
+            //ScriptRuntime runtime = Python.CreateRuntime();
+            //var stu = new Student("LiLei", 30);
+            //dynamic module=runtime.UseFile("drawer.py");
+            //var m = new int[3] { 1, 2, 3 };
+            //module.printStu(m);
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Student
+    {
+        public Student(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get; set;
+        }
+        public int Age
+        {
+            get; set;
+        }
+    }
+}
--- InterpreterDemo/InteractiverConsole/drawer.py
+++ InterpreterDemo/InteractiverConsole/drawer.py
@@ -1,11 +1,7 @@
 #encoding=utf-8
-import sys
-sys.path.append(r"C:\Python27\Lib")
-sys.path.append(r"C:\Python27\Lib\site-packages\matplotlib")
-sys.path.append(r"C:\Python27\Lib\site-packages\matplotlib\pyplot.py")
-sys.path.append(r"C:\Python27\Lib\site-packages\numpy")
 
 import matplotlib.pyplot as pl
+#import matplotlib.pylab as pl
 import numpy as np
 
 pl.figure(1,figsize=(8,6))
@@ -15,7 +11,6 @@ pl.sca(ax1)
 plot1=pl.plot(x,np.sin(x),label='sin')
 plot2=pl.plot(x,np.cos(x),label='cos')
 pl.legend()
-#pl.show()
 
 pl.figure(2)
 pl.title('W=BMI*H*H')
@@ -26,9 +21,3 @@ pl.ylim(40,100)
 pl.plot([1.5,1.7,1.6],[60,70,60])
 
 pl.show()
-
-
-
-def  printStu(stu):
-    print 'Name:%s' %stu.Name
-    print 'Age:%d' %stu.age
