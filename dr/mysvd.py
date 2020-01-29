463c0ffbc52874622af7c2d6d6ba133261294d4d
dhl_zf <dhl_345@qq.com>
2020-01-29 20:34:53 +0800

    添加svd计算逻辑

---
+++ dr/mysvd.py
@@ -0,0 +1,97 @@
+import numpy as np
+
+# A=UΣV'  AA'=UΣΣ'U'=UDU'  A'A=VΣ'ΣV'=VDV'
+# 其中D=ΣΣ'
+# 相当于 
+# AA'进行特征值分解，特征向量U 特征值ΣΣ'
+# A'A进行特征值分解，特征向量V 特征值ΣΣ'
+def svd4mat(mat):
+    eigVals,eigVects=np.linalg.eig(mat)
+    # 按照特征值大小倒序排列
+    eigIndices1=np.argsort(eigVals)[::-1]
+    eigVals=eigVals[eigIndices1]
+    # 开平方为特征值
+    sigma=np.sqrt(eigVals)
+    # 重新排列特征向量
+    reconVects=eigVects[:,eigIndices1]
+    return sigma,reconVects
+def svd(data):
+    m,n=data.shape
+    data=data/255
+    sigma1,U=svd4mat(np.dot(data,data.T))
+    sigma2,V=svd4mat(np.dot(data.T,data))
+    sigma=sigma2 if m>n else sigma1
+    sigma=sigma*255
+    return U,sigma,V.T
+
+# 方法2:
+# U=A*Inv(ΣV') ===> U=AV*Inv(Σ)
+# V'=Inv(UΣ)*A ===> V'=Inv(Σ)*U'A
+def mysvd_u(data):
+    m,n=data.shape
+    mat=np.dot(data,data.T)
+    eigVals,eigVects=np.linalg.eig(mat)
+    # print(eigVals,eigVects)
+    eigValIndices=np.argsort(eigVals)[::-1]
+    # print(eigValIndices)
+    eigVals=eigVals[eigValIndices]
+    # print(eigVals)
+    U=eigVects[:,eigValIndices]
+    sigma=np.sqrt(eigVals)
+    diag=np.diag(sigma)
+    sigmaInv=np.linalg.inv(diag)
+    VT=sigmaInv.dot(U.T).dot(data)
+    return U,sigma[:n],VT[:,:n]
+def mysvd_v(data):
+    m,n=data.shape
+    mat=np.dot(data.T,data)
+    eigVals,eigVects=np.linalg.eig(mat)
+    # print(eigVals,eigVects)
+    eigValIndices=np.argsort(eigVals)[::-1]
+    # print(eigValIndices)
+    eigVals=eigVals[eigValIndices]
+    # print(eigVals)
+    V=eigVects[:,eigValIndices]
+    sigma=np.sqrt(eigVals)
+    diag=np.diag(sigma)
+    sigmaInv=np.linalg.inv(diag)
+    U=data.dot(V).dot(sigmaInv)
+    VT=V.T
+    return U[:,:m],sigma[:m],VT
+def mysvd(data):
+    m,n=data.shape
+    data=float(data)/255
+    if(m>=n):
+        U,sigma,VT=mysvd_u(data)
+    else:
+        U,sigma,VT=mysvd_v(data)
+    sigma=int(sigma*255)
+    return sigma
+
+if __name__=="__main__":
+    dataset=np.array([[1.0,2.0,3.0],[2,3.9,6.1],[3,6.01,9.03],[4,8.01,12.04]])
+    print("from my svd:")
+    u,sigma,vT=svd(dataset)
+    print('u:',u)
+    print("sigma",sigma)
+    print('v:',vT)
+
+    u,sigma,vT=svd(dataset.T)
+    print('u:',u)
+    print("sigma",sigma)
+    print('v:',vT)
+
+    print("\rfrom linalg svd:")
+
+    u,sigma,vT=np.linalg.svd(dataset)
+    print('u:',u)
+    print("sigma",sigma)
+    print('v:',vT)
+
+    u,sigma,vT=np.linalg.svd(dataset.T)
+
+    print('u:',u)
+    print("sigma",sigma)
+    print('v:',vT)
+
+
--- dr/svd.py
+++ dr/svd.py
@@ -1,35 +1,50 @@
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
+import matplotlib.image as pimg
+import dr.mysvd as mydr
 # from sklearn import datasets
+import time
 from skimage import io
 import os
 dirName=dirName=os.path.dirname(__file__)
 
 
-# def getImgAsMat(index):
-#     ds = datasets.fetch_olivetti_faces()
-#     return np.mat(ds.images[index])
-
 def getImgAsMatFromFile(filename):
-    img = io.imread(filename,as_gray=True)
-    return np.mat(img) 
+    img=pimg.imread(filename)
+    # print(img.shape)
+    return img
 
-def plotImg(imgMat):
-    plt.imshow(imgMat, cmap=plt.cm.gray)
+
+def plotImg2(*args):
+    fig=plt.figure(figsize=(16,9))     
+    for i in range(len(args)):
+        ax=fig.add_subplot(3,3,(2*i+1))
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_title('{0} eigens'.format(args[i][0]))
+        ax.imshow(args[i][1].astype(np.uint8))
     plt.show()
 
 def recoverBySVD(imgMat, k):
+    img_tmp=imgMat.reshape(1440,1080*3)
     # singular value decomposition
-    U, s, V = la.svd(imgMat)
+    # start1=time.time()
+    # U, s, VT = mydr.svd(img_tmp)
+    # stop1=time.time()
+    # print("mysvd time:{0}".format(stop1-start1))
+    start2=time.time()
+    U, s, VT = np.linalg.svd(img_tmp)
+    stop2=time.time()
+    print("linalg.svd time:{0}".format(stop2-start2))
     # choose top k important singular values (or eigens)
     Uk = U[:, 0:k]
     #SK:二维数组
     Sk = np.diag(s[0:k])
-    Vk = V[0:k, :]
+    Vk = VT[0:k, :]
     # recover the image
-    imgMat_new = Uk * Sk * Vk
-    return imgMat_new
+    imgMat_new = Uk.dot(Sk).dot(Vk)
+    return imgMat_new.reshape(1440,1080,3)
 
 
 # -------------------- main --------------------- #
@@ -39,7 +54,12 @@ def recoverBySVD(imgMat, k):
 #plotImg(A_new)
 if __name__=="__main__":
     A = getImgAsMatFromFile('./dr/data/niu.jpg')
-    plotImg(A)
-    A_new = recoverBySVD(A, 50)
-    plotImg(A_new)
+    A_new30 = recoverBySVD(A, 30)
+    A_new50 = recoverBySVD(A, 50)
+    A_new100 = recoverBySVD(A, 100)
+    A_new120 = recoverBySVD(A, 120)
+    A_new150 = recoverBySVD(A, 150)
+    plotImg2(*(('30',A_new30),('50',A_new50),('100',A_new100),('120',A_new120),('150',A_new150)))
+
+    
 
